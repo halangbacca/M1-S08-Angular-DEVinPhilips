d409ad5b8b5c796bebe61a95816e04e15994dc64
Halan <halanbacca@outlook.com>
2022-11-30 10:55:43 -0300

    ngFor

--- src/app/components/notification-list/notification-list.component.ts
+++ src/app/components/notification-list/notification-list.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
-  styleUrls: ['./notification-list.component.scss']
+  styleUrls: ['./notification-list.component.scss'],
 })
 export class NotificationListComponent {
+  jogos = [
+    {
+      titulo: 'Resident Evil',
+      descricao:
+        'Enfrente armas biológicas criadas por corporações farmacêuticas ou grupos terroristas nesse clássico do Survival Horror',
+      data: 'há 10 minutos',
+    },
+    {
+      titulo: 'Silent Hill',
+      descricao:
+        'Enfrente seus piores pesadelos em Silent Hill, um clássico do terror psicológico',
+      data: 'há 20 minutos',
+    },
+    {
+      titulo: 'Dead Space',
+      descricao:
+        'O jogador controla o engenheiro chamado Isaac Clarke, que luta contra uma infestação alienígena que transforma humanos em monstros alienígenas chamados Necromorphs, a bordo de uma nave de mineração espacial chamada USG Ishimura',
+      data: 'há 30 minutos',
+    },
+  ];
 
+  salvarPessoa() {
+    console.log('Cadastrado com sucesso');
+  }
 }
