fbc8ecd05db1a1bef078f2f261d4565c2ff8efe1
Halan <halanbacca@outlook.com>
2022-12-01 14:51:35 -0300

    ngClass

--- src/app/components/notification-list/notification-list.component.ts
+++ src/app/components/notification-list/notification-list.component.ts
@@ -12,22 +12,21 @@ export class NotificationListComponent {
       descricao:
         'Enfrente armas biológicas criadas por corporações farmacêuticas ou grupos terroristas nesse clássico do Survival Horror',
       data: 'há 10 minutos',
+      lido: false,
     },
     {
       titulo: 'Silent Hill',
       descricao:
         'Enfrente seus piores pesadelos em Silent Hill, um clássico do terror psicológico',
       data: 'há 20 minutos',
+      lido: false,
     },
     {
       titulo: 'Dead Space',
       descricao:
         'O jogador controla o engenheiro chamado Isaac Clarke, que luta contra uma infestação alienígena que transforma humanos em monstros alienígenas chamados Necromorphs, a bordo de uma nave de mineração espacial chamada USG Ishimura',
       data: 'há 30 minutos',
+      lido: false,
     },
   ];
-
-  salvarPessoa() {
-    console.log('Cadastrado com sucesso');
-  }
 }
